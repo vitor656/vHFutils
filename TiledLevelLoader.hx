48b19ac2fca42dff31eda22ed2d5b7d058ecd60b
Vitor <vitor.rabelo.656@gmail.com>
2019-04-07 11:23:54 -0300

    Making TiledLevelLoader more generic

--- TiledLevelLoader.hx
+++ TiledLevelLoader.hx
@@ -1,13 +1,28 @@
 package vHFutils;
 
+import flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling;
+import flixel.system.FlxAssets.FlxTilemapGraphicAsset;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
 import flixel.FlxState;
 import flixel.addons.editors.tiled.TiledMap;
 import flixel.addons.editors.tiled.TiledObjectLayer;
 import flixel.addons.editors.tiled.TiledTileLayer;
-import flixel.system.FlxAssets.FlxTilemapGraphicAsset;
 import flixel.tile.FlxTilemap;
 import flixel.addons.editors.tiled.TiledObject;
 import flixel.FlxG;
+
+typedef MapLayer = { 
+	layerName : String, 
+	isCollidable : Bool, 
+	tileSet : FlxTilemapGraphicAsset, 
+	autotile : FlxTilemapAutoTiling 
+}
+
+typedef EntitiesLayer = { 
+	layerName : String, 
+	type : Class<FlxSprite> 
+}
 
 // This class has to be used for Tiled based levels (.tmx)
 class TiledLevelLoader 
@@ -15,147 +30,90 @@ class TiledLevelLoader
 
 	public static var currentLevel : String;
 	public static var currentLevelPath : String;
-	public static var currentTiledMap : TiledMap;
-	public static var currentCollidableMap : FlxTilemap;
-	public static var tilesAsset : String = "";
+	private static var currentTiledMap : TiledMap;
+	private static var currentCollidableMap : FlxTilemap;
 
-	public inline static var LAYER_MAIN : String = "main";
-	public inline static var LAYER_BACKGROUND : String = "background";
-	public inline static var LAYER_FOREGROUND : String = "foreground";
-	public inline static var LAYER_PLAYER : String = "player";
-	public inline static var LAYER_ENEMIES : String = "enemies";
-	public inline static var LAYER_COLLECTABLES : String = "collectables";
+	private static var tilesLayers : Array<MapLayer> = new Array<MapLayer>();
+	private static var entitiesLayers : Array<EntitiesLayer> = new Array<EntitiesLayer>();
 
 	public static function loadLevel(state:FlxState, level:String)
 	{
 		currentLevel = level;
 		currentLevelPath = "assets/data/" + level + ".tmx";
-		
-		if (setupTilesAsset(level)){
-			
-			var tiledMap = new TiledMap(currentLevelPath);
 
-			var backMap = new FlxTilemap();
-			var backgroundLayer : TiledTileLayer = cast tiledMap.getLayer(LAYER_BACKGROUND);
-			if(backgroundLayer != null){	
-				backMap.loadMapFromArray(
-					backgroundLayer.tileArray, 
-					tiledMap.width, 
-					tiledMap.height, 
-					tilesAsset, 
-					tiledMap.tileWidth, 
-					tiledMap.tileHeight, 
-					1
+		var tiledMap = new TiledMap(currentLevelPath);
+
+		for(layer in tilesLayers) {
+			var layerMap = new FlxTilemap();
+			var tiledLayer : TiledTileLayer = cast tiledMap.getLayer(layer.layerName);
+
+			if(tiledLayer != null)
+			{
+				layerMap.loadMapFromArray(
+					tiledLayer.tileArray,
+					tiledMap.width,
+					tiledMap.height,
+					layer.tileSet,
+					tiledMap.tileWidth,
+					tiledMap.tileHeight,
+					layer.autotile
 				);
-				backMap.solid = false;
 
-				state.add(backMap);
-			}
+				layerMap.solid = layer.isCollidable;
 
-		
-			var mainLayer : TiledTileLayer = cast tiledMap.getLayer(LAYER_MAIN);
-			if(mainLayer != null){
-				currentCollidableMap = new FlxTilemap();
-				currentCollidableMap.loadMapFromArray(
-					mainLayer.tileArray, 
-					tiledMap.width, 
-					tiledMap.height, 
-					tilesAsset, 
-					tiledMap.tileWidth, 
-					tiledMap.tileHeight, 
-					1
-				);
-				state.add(currentCollidableMap);
+				if (layer.isCollidable)
+				{
+					currentCollidableMap = layerMap;
+				}
+				
+				state.add(layerMap);
 			} else {
-				trace("Tiled: Main layer not found.");
+				trace(layer.layerName + " layer not found.");
 			}
-			
-			
-			var foreMap = new FlxTilemap();
-			var foregroundLayer : TiledTileLayer = cast tiledMap.getLayer(LAYER_FOREGROUND);
-			if(foregroundLayer != null){
-				foreMap.loadMapFromArray(
-					foregroundLayer.tileArray, 
-					tiledMap.width, 
-					tiledMap.height, 
-					tilesAsset, 
-					tiledMap.tileWidth, 
-					tiledMap.tileHeight, 
-					1
-				);
-				foreMap.solid = false;
-
-				state.add(foreMap);
-			}
-
-			FlxG.camera.setScrollBoundsRect(0, 0, tiledMap.fullWidth, tiledMap.fullHeight, true);
-
-			currentTiledMap = tiledMap;
-			
-			//Spawn all objects
-			spawnObjects(state, currentTiledMap);
-		} else {
-			trace("Tiles Asset not found");
 		}
+
+		FlxG.camera.setScrollBoundsRect(0, 0, tiledMap.fullWidth, tiledMap.fullHeight, true);
+
+		currentTiledMap = tiledMap;
+		
+		//Spawn all objects
+		placeEntities(state, currentTiledMap);
+		
 	}
 	
-	public static function spawnObjects(state:FlxState, map:TiledMap) : Void
+	public static function placeEntities(state:FlxState, map:TiledMap) : Void
 	{
-		spawnPlayer(state, map);
-		spawnEnemies(state, map);
-		spawnCollectables(state, map);
-	}
+		for(layer in entitiesLayers){
+			var arrEntities : Array<TiledObject> = getLevelEntities(map, layer);
+			if(arrEntities != null && arrEntities.length > 0){
 
-	public static function spawnPlayer(state:FlxState, map:TiledMap) : Void
-	{
-		var arrPlayer = getLevelObjects(map, LAYER_PLAYER);
-		if(arrPlayer != null && arrPlayer.length > 0){
-			var playerObject : TiledObject = arrPlayer[0];
+				for(entity in arrEntities){
 
-			//Choose Player class to instantiate
-			//(cast state).player = new Player(playerObject.x, playerObject.y);
-			//state.add((cast state).player);
-		}
-	}
+					spawnEntity(state, entity, layer.type);
 
-	public static function spawnEnemies(state:FlxState, map:TiledMap) : Void
-	{
-		var arrEnemies : Array<TiledObject> = getLevelObjects(map, LAYER_ENEMIES);
-		if(arrEnemies != null && arrEnemies.length > 0){
-
-			for(enemyObject in arrEnemies){
-				switch(enemyObject.type){
-					case "type_1":
-					break;
-					case "type_2":
-					break;
 				}
-			}
 
+			}
 		}
+
 	}
 
-	public static function spawnCollectables(state:FlxState, map:TiledMap) : Void
+	private static function spawnEntity(state : FlxState, entity : TiledObject, type : Class<FlxSprite>) : Void 
 	{
-		var arrCollectables : Array<TiledObject> = getLevelObjects(map, LAYER_COLLECTABLES);
-		if(arrCollectables != null && arrCollectables.length > 0){
+		var spawnPosition : FlxPoint = new FlxPoint(entity.x, entity.y);
+		var newEntity = Type.createInstance(type, []);
 
-			for(collectableObject in arrCollectables){
-				switch(collectableObject.type){
-					case "type_1":
-					break;
-					case "type_2":
-					break;
-				}
-			}
+		newEntity.x = spawnPosition.x;
+		newEntity.y = spawnPosition.y;
 
-		}
+		state.add(newEntity);
+
 	}
 	
-	public static function getLevelObjects(map:TiledMap, layer:String) : Array<TiledObject>
+	private static function getLevelEntities(map:TiledMap, layer:EntitiesLayer) : Array<TiledObject>
 	{
-		if (map != null && map.getLayer(layer) != null){
-			var objLayer : TiledObjectLayer = cast map.getLayer(layer);
+		if (map != null && map.getLayer(layer.layerName) != null){
+			var objLayer : TiledObjectLayer = cast map.getLayer(layer.layerName);
 			return objLayer.objects;
 		} else {
 			trace(layer + " layer not found.");
@@ -163,17 +121,22 @@ class TiledLevelLoader
 			return [];
 		}
 	}
-	
-	public static function setupTilesAsset(level : String) : Bool
+
+	public static function registerTilesLayer(layer : MapLayer, position : Int = -1) : Void
 	{
-		switch(level){
-			case 'level':
-				tilesAsset = AssetPaths.lowrez__png;
+		if(position != -1) {
+			tilesLayers.insert(position, layer);
+		} else {
+			tilesLayers.push(layer);
 		}
-		
-		if (tilesAsset != null && tilesAsset != "")
-			return true;
-		
-		return false;
+	}
+
+	public static function registerEntitiesLayer(layer : EntitiesLayer, position : Int = -1) : Void
+	{
+		if(position != -1) {
+			entitiesLayers.insert(position, layer);
+		} else {
+			entitiesLayers.push(layer);
+		}
 	}
 }
